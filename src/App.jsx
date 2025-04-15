c7292c97606dbe0c1e25d7f96bf239a472662eab
SouaJenni <jennifervialli1@gmail.com>
2025-04-15 15:52:07 -0300

    Adicionando tabela para a exibir os dados

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import {useNavigate} from 'react-router-dom';
 import {useDispatch, useSelector} from 'react-redux';
 
 import {Button, H1, H2} from '@blueprintjs/core';
+import {Cell, Column, Table2} from '@blueprintjs/table';
+import '@blueprintjs/table/lib/css/table.css';
 
 import {buscarPessoas} from './actions.js';
 import './App.css';
@@ -17,13 +19,29 @@ function App() {
     }, []);
 
     return (
-        <>
+        <div style={{padding: '20px'}}>
             <H1>Pessoas:</H1>
-            {pessoas.map(pessoa =>(
-                <H2 key={pessoa.id}>{pessoa.nome}-{pessoa.idade}</H2>
-            ))}
+            <div style={{height: `${pessoas.length * 40+40}px`, width: '100%', marginBottom: '20px', overflow: 'hidden'}}>
+                <Table2
+                    numRows={pessoas.length}
+                    enableRowHeader={false}
+                    enableColumnResizing={false}
+                    defaultColumnWidth={200}
+                    rowHeights={Array(pessoas.length).fill(40)}
+                >
+                    <Column
+                        name='Nome'
+                        cellRenderer={rowIndex => <Cell>{pessoas[rowIndex].nome}</Cell>}
+                    />
+                    <Column
+                        name='Idade'
+                        cellRenderer={rowIndex => <Cell>{pessoas[rowIndex].idade}</Cell>}
+                    />
+                </Table2>
+            </div>
             <Button text={'Cadastrar Pessoa'} onClick={() => navigator('/cadastrar')}></Button>
-        </>
+
+        </div>
     );
 }
 
